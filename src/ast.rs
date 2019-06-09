8ef857fbfe78638ee03ebf545667275a63413f6e
Matthew Constable <bonsty@gmail.com>
2019-06-09 21:23:31 +0100

    Refactored AST printer.

--- src/ast.rs
+++ src/ast.rs
@@ -28,28 +28,26 @@ pub struct LiteralExp {
     value: Literal
 }
 
-fn ast_printer<'a>(builder: &'a mut String, exp: &Exp) -> &'a String {
-    // TODO MC: Commonize parantheses function without borrow checker being sad.
+fn ast_printer<'a>(builder: &'a mut String, exp: &'a Exp) -> &'a String {
+    fn add_parens<'a>(builder: &'a mut String, name: String, exprs: Vec<&Exp>) -> &'a String {
+        builder.push_str(format!("({}", name).as_str());
+        for exp in exprs {
+            let sub_string = &mut String::new();
+            let sub_builder = ast_printer(sub_string, exp);
+            builder.push_str(format!(" {}", sub_builder).as_str())
+        }
+        builder.push(')');
+        builder
+    }
     match exp {
         Exp::BinaryExp(x) => {
-            builder.push_str(format!("({} ", x.operator.lexeme).as_str());
-            ast_printer(builder, x.left.borrow());
-            builder.push(' ');
-            ast_printer(builder, x.right.borrow());
-            builder.push(')');
-            builder
+            add_parens(builder,x.operator.lexeme.clone(), vec![x.left.borrow(), x.right.borrow()])
         },
         Exp::GroupingExp(x) => {
-            builder.push_str("(group ");
-            ast_printer(builder, x.exp.borrow());
-            builder.push(')');
-            builder
+            add_parens(builder,"group".to_string(), vec![x.exp.borrow()])
         },
         Exp::UnaryExp(x) => {
-            builder.push_str(format!("({} ", x.operator.lexeme).as_str());
-            ast_printer(builder, x.right.borrow());
-            builder.push(')');
-            builder
+            add_parens(builder,x.operator.lexeme.clone(), vec![x.right.borrow()])
         },
         Exp::LiteralExp(x) => {
             builder.push_str((match x.value.clone() {
@@ -67,12 +65,17 @@ mod tests {
     use super::*;
     use scanner::TokenType;
 
+    // TODO MC: Actually test unary, and identifier literal - probably fine for now
     #[test]
     fn ast_printer_simple()
     {
-        let binary_exp = Exp::BinaryExp(
+        let binary_exp: Exp = Exp::BinaryExp(
             BinaryExp {
-                left: Box::new(Exp::LiteralExp(LiteralExp{value: Literal::NUMBER(1.2)})),
+                left: Box::new(Exp::GroupingExp(
+                    GroupingExp{
+                        exp: Box::new(Exp::LiteralExp(LiteralExp{ value: Literal::STRING("foobar".to_string()) }))
+                    }
+                )),
                 operator: Token {
                     token_type: TokenType::EqualEqual,
                     lexeme: "==".to_string(),
@@ -82,6 +85,6 @@ mod tests {
         let mut output_string = String::new();
         let output = ast_printer(&mut output_string, &binary_exp);
 
-        assert_eq!(*output, "(== 1.2 1.5)".to_string());
+        assert_eq!(*output, "(== (group foobar) 1.5)".to_string());
     }
 }
